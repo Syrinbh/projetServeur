ed4971eb511d1de13e8a1e9536fcbb7b9b9b3412
= <=>
2025-04-03 11:01:30 +0200

    ajout trucs forms.py

--- Serveur/projetServeur/tasks/forms.py
+++ Serveur/projetServeur/tasks/forms.py
@@ -2,8 +2,14 @@
 
 from django.contrib.auth.forms import UserCreationForm 
 from .models import User
+from django import forms 
+
 class RegisterForm (UserCreationForm ):
     class Meta : 
         model = User 
         fields = ['username', 'email', 'description', 'password1', 'password2' ]
         
+class Taskform(forms.ModelForm):
+    class Meta :
+        model = User
+        fields = ['title', 'statut', 'description', 'parentTask', 'sonTask' ]
