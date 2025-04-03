640fa121a5d3528d8e2c8fde24b995673fba3771
JB <j-baptiste.largeron@etu.uca.fr>
2025-04-03 09:07:11 +0200

    register_view

--- Serveur/projetServeur/tasks/views.py
+++ Serveur/projetServeur/tasks/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .forms import RegisterForm
 
 def Register_view(request):
-    return HttpResponse("Page d'inscription")
+    if request.method == 'POST' :
+        form = RegisterForm(request.POST)
+        if (form.is_valid()):
+            user =form.save()
+            login(request,user)
+            return redirect('login')
+
+    return render(request, "tasks/register.html",{'form': form})
+
+
 
 def Login_view(request):
+    # if request.method == 'POST' :
+        
+
     return HttpResponse("Page de connexion")
