35567a784f9ff39d1299bd7863ba658df3f7448c
JB <j-baptiste.largeron@etu.uca.fr>
2025-04-17 10:21:11 +0200

    je crois que ca marche

--- Serveur/projetServeur/tasks/forms.py
+++ Serveur/projetServeur/tasks/forms.py
@@ -7,7 +7,7 @@ from django import forms
 
 class RegisterForm (UserCreationForm ):
     class Meta : 
-        model = User 
+        model = User
         fields = ['username', 'email', 'description', 'password1', 'password2' ]
         
 class Taskform(forms.ModelForm):
