c02373233fe41b0c7bf5f0cc08ba01c403ec57ed
Syrine BEN HASSINE <syrine.ben_hassine@etu.uca.fr>
2025-05-20 16:31:34 +0200

    modify urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,10 +4,6 @@ from django.urls import path
 from . import views
 from tasks import views
 
-#from . import views
- #il y'a espace apres le point , sinon erreur 
-
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.Register_view, name = 'register'),
